7513a7e78e7eb11e7417cfc2c61a21bae78db95e
Pranav Marla <pranav.a.marla@gmail.com>
2023-04-07 00:56:29 -0400

    Replacing multiple small print with one big print did not fix input bug -- might  have made it worse actually

--- quantum-wordle-code.py
+++ quantum-wordle-code.py
@@ -713,22 +713,11 @@ def run_game(classical_attempt_option=CLASSICAL_ATTEMPT_OPTION, quantum_attempt_
             # Eg. Attempt 1 is located at index 0
             current_attempt = attempts_list[attempt_num - 1]
             # User can choose what to do as long as they haven't run out of attempts
-
-            #! DEBUG
-
-            # print('\nSelect an option by entering the corresponding number:')
-            # print(f'{classical_attempt_option}: Classical attempt (1 guess)')
-            # print(f'{quantum_attempt_option}: Quantum attempt (superposition of 2 guesses)')
-            # print(f'{measure_option}: Measure all quantum attempts (collapse to classical)')
-            # print(f'{exit_option}: Exit')
-
-            print(
-                    '\nSelect an option by entering the corresponding number:\n'
-                    f'{classical_attempt_option}: Classical attempt (1 guess)\n'
-                    f'{quantum_attempt_option}: Quantum attempt (superposition of 2 guesses)\n'
-                    f'{measure_option}: Measure all quantum attempts (collapse to classical)\n'
-                    f'{exit_option}: Exit'
-                )
+            print('\nSelect an option by entering the corresponding number:')
+            print(f'{classical_attempt_option}: Classical attempt (1 guess)')
+            print(f'{quantum_attempt_option}: Quantum attempt (superposition of 2 guesses)')
+            print(f'{measure_option}: Measure all quantum attempts (collapse to classical)')
+            print(f'{exit_option}: Exit')
             
             # After printing above options, print error message if user previously made an invalid choice
             if user_entered_invalid_choice:
