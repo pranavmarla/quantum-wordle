31cd263c8ef9f3fbb633c9fd77a48bc1819b058b
Pranav Marla <pranav.a.marla@gmail.com>
2023-04-08 21:56:55 -0400

    Replace keyboard order with alphabetical order to be more intuitive

--- quantum-wordle-code.py
+++ quantum-wordle-code.py
@@ -62,9 +62,9 @@ QUANTUM_BACKEND = Aer.get_backend('qasm_simulator')
 # List of letters that have not yet been used in any guess
 # Note that, for ease of use, the letters are in "keyboard order" (i.e. the same order as a computer keyboard)
 AVAILABLE_LETTERS = [
-    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
-        'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-            'Z', 'X', 'C', 'V', 'B', 'N', 'M'
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     ]
 
 
